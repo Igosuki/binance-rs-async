6d87339c465dec504e430f9a49f92bd3da2676c8
Guillaume Balaine <igosuki@gmail.com>
2021-06-24 18:00:32 +0200

    There are two separate ws endpoints, so change config accordingly between multi stream and ws (by default) endpoints

--- src/config.rs
+++ src/config.rs
@@ -13,10 +13,10 @@ impl Config {
     pub fn default() -> Config {
         Config {
             rest_api_endpoint: "https://api.binance.com".into(),
-            ws_endpoint: "wss://stream.binance.com:9443/ws".into(),
+            ws_endpoint: "wss://stream.binance.com:9443".into(),
 
             futures_rest_api_endpoint: "https://fapi.binance.com".into(),
-            futures_ws_endpoint: "wss://fstream.binance.com".into(),
+            futures_ws_endpoint: "wss://fstream.binance.com:9443".into(),
 
             recv_window: 5000,
         }
@@ -25,9 +25,9 @@ impl Config {
     pub fn testnet() -> Config {
         Config::default()
             .set_rest_api_endpoint("https://testnet.binance.vision")
-            .set_ws_endpoint("wss://testnet.binance.vision/ws")
+            .set_ws_endpoint("wss://testnet.binance.vision")
             .set_futures_rest_api_endpoint("https://testnet.binancefuture.com")
-            .set_futures_ws_endpoint("https://testnet.binancefuture.com/ws")
+            .set_futures_ws_endpoint("wss://testnet.binancefuture.com")
     }
 
     pub fn set_rest_api_endpoint<T: Into<String>>(mut self, rest_api_endpoint: T) -> Self {
