9ca572c29a5ead75633c2f4b85c23444ceea874b
Christian Drappi <christiandrappi@gmail.com>
2022-05-10 11:09:46 -0400

    do it

--- src/delivery/account.rs
+++ src/delivery/account.rs
@@ -4,7 +4,6 @@ use crate::errors::*;
 use crate::futures::rest_model::{Transaction, OrderType, CanceledOrder};
 use crate::rest_model::{OrderSide, TimeInForce, PairQuery};
 use crate::futures::account::OrderRequest;
-use crate::util::*;
 
 
 #[derive(Clone)]
