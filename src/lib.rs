727fcf94993098f3cdf0c530bfe6f7c984412f61
MrConvexity <jamest7117@gmail.com>
2022-07-18 17:56:25 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -60,7 +60,7 @@
 //! - Errors are implemented using [![thiserror]](https://docs.rs/thiserror/1.0.25/thiserror/)
 //!
 
-#![deny(unstable_features, unused_must_use, unused_mut, unused_imports, unused_import_braces)]
+#![warn(unstable_features, unused_must_use, unused_mut, unused_imports, unused_import_braces)]
 
 #[macro_use]
 extern crate lazy_static;
