28d108fdea7fd67fcd65ce27e8b02547cdde5c76
Guillaume Balaine <igosuki@gmail.com>
2023-01-12 11:32:41 +0100

    fix: config documentation

--- src/config.rs
+++ src/config.rs
@@ -49,32 +49,124 @@ impl Config {
             .set_futures_ws_endpoint("wss://testnet.binancefuture.com")
     }
 
+    /// Sets the rest api endpoint. Defaults to "https://testnet.binance.vision".
+    ///
+    /// # Arguments
+    ///
+    /// * `futures_rest_api_endpoint`:
+    ///
+    /// returns: Config
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use binance::config::Config;
+    /// let config = Config::default();
+    /// config.set_rest_api_endpoint("http://myendpoint:8080");
+    /// ```
     pub fn set_rest_api_endpoint<T: Into<String>>(mut self, rest_api_endpoint: T) -> Self {
         self.rest_api_endpoint = rest_api_endpoint.into();
         self
     }
 
+    /// Sets the websocket endpoint. Defaults to "wss://testnet.binance.vision".
+    ///
+    /// # Arguments
+    ///
+    /// * `futures_ws_endpoint`:
+    ///
+    /// returns: Config
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use binance::config::Config;
+    /// let config = Config::default();
+    /// config.set_ws_endpoint("ws://myendpoint:8080");
+    /// ```
     pub fn set_ws_endpoint<T: Into<String>>(mut self, ws_endpoint: T) -> Self {
         self.ws_endpoint = ws_endpoint.into();
         self
     }
+
+    /// Sets the futures rest api endpoint. Defaults to "https://testnet.binancefuture.com".
+    ///
+    /// # Arguments
+    ///
+    /// * `futures_rest_api_endpoint`:
+    ///
+    /// returns: Config
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use binance::config::Config;
+    /// let config = Config::default();
+    /// config.set_futures_rest_api_endpoint("http://myendpoint:8080");
+    /// ```
     pub fn set_futures_rest_api_endpoint<T: Into<String>>(mut self, futures_rest_api_endpoint: T) -> Self {
         self.futures_rest_api_endpoint = futures_rest_api_endpoint.into();
         self
     }
 
+    /// Sets the futures websocket endpoint. Defaults to "wss://testnet.binancefuture.com".
+    ///
+    /// # Arguments
+    ///
+    /// * `futures_ws_endpoint`:
+    ///
+    /// returns: Config
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use binance::config::Config;
+    /// let config = Config::default();
+    /// config.set_futures_ws_endpoint("ws://myendpoint:8080");
+    /// ```
     pub fn set_futures_ws_endpoint<T: Into<String>>(mut self, futures_ws_endpoint: T) -> Self {
         self.futures_ws_endpoint = futures_ws_endpoint.into();
         self
     }
 
+    /// Sets the 'receive window'. The receive window is the number of milliseconds after timestamp
+    /// the request is valid for.
+    ///
+    /// # Arguments
+    ///
+    /// * `recv_window`: The receive window, in milliseconds. Defaults to 5000.
+    ///
+    /// returns: Config
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use binance::config::Config;
+    /// let config = Config::default();
+    /// config.recv_window(300);
+    /// ```
     pub fn set_recv_window(mut self, recv_window: u64) -> Self {
         self.recv_window = recv_window;
         self
     }
 
-    pub fn set_timeout(mut self, timout: u64) -> Self {
-        self.timeout = timout;
+    /// Sets the client timeout
+    ///
+    /// # Arguments
+    ///
+    /// * `timeout`: The timeout, in seconds
+    ///
+    /// returns: Config
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use binance::config::Config;
+    /// let config = Config::default();
+    /// config.set_timeout(3);
+    /// ```
+    pub fn set_timeout(mut self, timeout: u64) -> Self {
+        self.timeout = timeout;
         self
     }
 }
