16180443f3262d7879501367d4629231d0bf9f11
Bogdan Vasilishin <togetherwithra@gmail.com>
2024-07-05 14:11:40 +0300

    + New execution type AMENDMENT

--- src/rest_model.rs
+++ src/rest_model.rs
@@ -1238,6 +1238,8 @@ pub enum ExecutionType {
     Expired,
     /// The order has expired due to STP trigger.
     TradePrevention,
+    /// Order modified
+    Amendment,
 }
 
 /// Status of an order, this can typically change over time
